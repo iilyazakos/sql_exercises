7892fd69b845d345b9422bd39fe79a60284c4610
Ilya Nenartovich <iilyazakos@gmail.com>
2021-11-06 20:47:34 +0300

    update code-style

--- task_7.sql
+++ task_7.sql
@@ -3,12 +3,9 @@
 имени (по возрастанию) список пассажиров, совершивших хотя бы 1 полет.
 */
 
-
-
-SELECT name, COUNT(*) AS count  
-FROM Passenger
-JOIN Pass_in_trip
-    ON Passenger.id=Pass_in_trip.passenger
-GROUP BY passenger
-HAVING COUNT(trip) > 0
-ORDER BY COUNT(trip) DESC, name;
+select name, count(trip) as count  
+from Passenger
+join Pass_in_trip on Passenger.id=Pass_in_trip.passenger
+group by passenger
+having count(trip) > 0
+order by count(trip) desc, name;
