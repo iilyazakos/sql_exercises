470a0b946d6a99af0f3077fee9b3c0efda11dba6
Ilya Nenartovich <iilyazakos@gmail.com>
2021-11-06 20:42:47 +0300

    update code-style

--- task_5.sql
+++ task_5.sql
@@ -2,13 +2,11 @@
 Сколько и кто из семьи потратил на развлечения (entertainment). Вывести статус в семье, имя, сумму
 */
 
-SELECT fm.status, fm.member_name, SUM(p.amount*p.unit_price) AS costs
-FROM FamilyMembers AS fm
-JOIN Payments AS p
-    ON fm.member_id=p.family_member
-JOIN Goods AS g
-    ON p.good=g.good_id
-JOIN GoodTypes AS gt
-    ON g.type=gt.good_type_id
-WHERE good_type_name = 'entertainment'
-GROUP BY fm.status, fm.member_name;
+select fm.status, fm.member_name,
+       sum(p.amount*p.unit_price) as costs
+from FamilyMembers fm
+join Payments p on fm.member_id = p.family_member
+join Goods g on p.good = g.good_id
+join GoodTypes gt on g.type=gt.good_type_id
+where good_type_name = 'entertainment'
+group by fm.status, fm.member_name;
